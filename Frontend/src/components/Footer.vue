ef96b85d5b4547ea178be241c8031f32df68d460
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-04-28 16:21:58 +0200

    Created Navbar and Footer and imported to App, removed HelloWorld.vue
    Co-Authored-By: Martin Isaksen <77095450+izaxen@users.noreply.github.com>

---
+++ Frontend/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+<footer>
+&copy; 2021 AMRT future production 
+
+</footer>  
+</template>
+
+<style>
+footer{
+  background-image: linear-gradient(
+    rgba(77, 224, 9, 0.787),
+    rgb(208, 255, 164)
+    
+  );
+  padding: 20px;
+}
+</style>
---
+++ Frontend/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <router-link to ="/">Home</router-link>
+    |
+    <router-link to="/my-page">My Page</router-link>
+   
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+nav {
+
+  padding: 10px;
+  background-image: linear-gradient(
+    rgb(208, 255, 164),
+    rgba(77, 224, 9, 0.787)
+  );
+
+box-shadow: 0 0 5px 5px;
+}
+a{
+  text-decoration: none;
+  color: rgb(0, 0, 0);
+  
+}
+
+a:hover{
+  text-decoration: underline;
+}
+
+
+</style>
