f2dde124457f33b5df3c17756ec7c8df89497c6c
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-04-28 11:59:08 +0200

    First commit

--- Backend/src/main/java/Main.java
+++ Backend/src/main/java/Main.java
@@ -1,14 +1,6 @@
-import express.Express;
-
-import static nosqlite.Database.collection;
-
 public class Main {
 
     public static void main(String[] args) {
-        Express app = new Express();
-
-        collection(op -> op.useBrowser = true);
-        app.listen(4000);
+        System.out.println("Hej");
     }
-
 }
--- src/main/java/dependencies/User.java
+++
@@ -1,2 +0,0 @@
-package dependencies;public class User {
-}
