4bedc3adef1dca37f370ab02dbcb0e41eb5cdff4
Martin Isaksen <izaxen@telia.com>
2021-04-29 17:43:01 +0200

    Added authorization and secure password

---
+++ Backend/src/main/java/models/Authorization.java
@@ -0,0 +1,73 @@
+package models;
+
+import utilities.HashPassword;
+import express.Express;
+import nosqlite.Database;
+
+import static nosqlite.Database.collection;
+import static nosqlite.Database.useBrowser;
+
+import java.util.Map;
+
+public class Authorization {
+    private Express app;
+
+    public Authorization(Express app){
+        this.app=app;
+        initAuthorization();
+    }
+
+    private void initAuthorization() {
+
+        app.post("/api/register", (req, res) -> {   //Create user
+            User user = req.body(User.class);
+
+            User exist = Database.collection("User").findOne("email==" + user.getEmail());
+            if (exist != null) {
+                res.json(Map.of("error", "Username already exist")); // Console variabel bara?
+                return;
+            }
+
+            String hashedPassword = HashPassword.hash(user.getPassword());
+            user.setPassword(hashedPassword);
+            Database.collection("User").save(user);
+
+            req.session("current-user", user);
+        });
+
+
+        app.post("/api/login", (req, res) -> {
+            User user = req.body(User.class);
+            User userInCollection = collection("User").findOne("email==" +user.getEmail());
+
+            if(userInCollection == null){
+                res.json(Map.of("error", "Control login details")); //Console Variable
+                return;
+            }
+
+            if(HashPassword.match(user.getPassword(), userInCollection.getPassword())){
+                req.session("current-user", userInCollection);
+
+                res.json(userInCollection);
+            }
+            else{
+                res.json(Map.of("error", "Control login details"));
+            }
+
+        });
+
+        app.get("/api/whoami", (req, res)-> {   //Control logged in user
+
+            res.json(req.session("current-user"));
+        });
+
+        app.get("/api/logout",(req,res)->{
+         req.session("current-user", null);
+
+         res.json(Map.of("ok", "Logged out"));
+        });
+
+
+    }
+    }
+
