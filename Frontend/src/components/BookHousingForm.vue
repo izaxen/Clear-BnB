c1723d688a9932cb0f8178b81c906b838ca6b5c0
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-04-29 20:53:35 +0200

    cleaned up

--- Frontend/src/components/BookHousingForm.vue
+++ Frontend/src/components/BookHousingForm.vue
@@ -1,17 +1,30 @@
 <template>
-  <input type="text" placeholder="calandar - From date" />
-  <input type="text" placeholder="calandar - To date" />
-  <NumberOfGuests :numOfDays="10" />
+  <div class="container">
+    <Calendar />
+    <NumberOfGuests :numOfDays="10" />
+  </div>
 </template>
 
 <script>
-import NumberOfGuests from '../components/NumberOfGuestsSelector.vue'
+import NumberOfGuests from './rentalPriceCalculator.vue'
+import Calendar from './Calendar.vue'
 
 export default {
   components: {
     NumberOfGuests,
+    Calendar,
   },
 }
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  padding: 10px;
+  width: 300px;
+  background: rgb(216, 216, 216);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 250px;
+}
+</style>
