f3752241bc402678610a9382b4b459b59e4a9cd5
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-04-29 20:52:53 +0200

    Addad a temp calendar component

---
+++ Frontend/src/components/Calendar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="date" />
+    <input type="date" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+div {
+  justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+}
+
+input {
+  width: 45%;
+  background: rgb(137, 136, 136);
+  -webkit-appearance: none;
+}
+</style>
