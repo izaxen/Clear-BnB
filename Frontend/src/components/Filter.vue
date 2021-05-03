8403ea92de5daec2787b99b252fba13effc41154
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-05-03 11:56:22 +0200

    First outline on a filter

---
+++ Frontend/src/components/Filter.vue
@@ -0,0 +1,39 @@
+<template>
+  <input v-model="textSearch" type="text" placeholder="search city..." />
+
+  <!-- <rentalObjects
+    v-for="object of rentalObjects"
+    v-bind:key="recipe.id" 
+    v-bind:recipe="recipe" 
+  /> -->
+</template>
+
+<script>
+export default {
+  components: {},
+
+  data() {
+    return {
+      textSearch: '',
+    }
+  },
+
+  computed: {
+    rentalObjects() {
+      if (this.textSearch) {
+        return this.$store.state.rentalObjects.filter((object) => {
+          let city = object.city.toLowerCase()
+          let search = this.textSearch.toLowerCase()
+          if (city.includes(search)) {
+            return recipe
+          }
+        })
+      } else {
+        return this.$store.state.rentalObjects
+      }
+    },
+  },
+}
+</script>
+
+<style scoped></style>
