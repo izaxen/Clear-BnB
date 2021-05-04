8ebcd7b2f7c9b61c87514806b141df446151ec64
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2021-05-04 09:37:45 +0200

    d

--- Frontend/src/components/SearchBar.vue
+++ Frontend/src/components/SearchBar.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="search-box">
-    <div class="box1"><input class="search-text" type="text" name="" placeholder="ruta 1"></div>
+  <form class="search-box">
+    <select @click="showCityNames" v-model="cityName" class="box1">
+      <option value="" disabled selected hidden>--Citys--</option>
+      <option v-for="city in citys" :value="city"> {{city}} </option>
+    </select>
       <Calendar :searchBar="searchBar" />
-    <div class="box4"><input class="search-text" type="text" name="" placeholder="ruta 4"></div>
+    <div class="box4" type="text" name="" placeholder="ruta 4">
+      <p>Persons</p>
+      </div>
+      <div class="person-div"></div>
     <a class="search-btn" href="#">
       <em class="fas fa-search"></em>
     </a>
-  </div>
+  </form>
 </template>
 
 <script>
@@ -19,6 +25,24 @@ components:{
 data(){
   return{
     searchBar: true,
+    city: '',
+    citys: [],
+    cityName: ''
+  }
+},
+
+methods: {
+  showCityNames(){
+    let getCitys = this.$store.state.rentalObjects;
+    for(let cityName of getCitys){
+
+      this.citys.push(cityName.city)
+    }
+
+    let removedDuplicates = this.citys.filter((value, index) => {
+      return this.citys.indexOf(value) === index
+    })
+    this.citys = removedDuplicates;
   }
 }
 
@@ -26,6 +50,15 @@ data(){
 </script>
 
 <style scoped>
+option {
+  color: rgb(0, 0, 0);
+  font-weight: 600;
+  background:#59deff93;
+  padding: 10px;
+  font-size: 17px;
+
+}
+
 .box1 {
   border-right: 1px solid gray;
 }
@@ -40,10 +73,8 @@ data(){
   padding: 10px;
   width: fit-content;
 }
-.search-box:focus-within > .search-text {
-  width: 350px;
-}
-.search-box:hover > .search-btn {
+
+.box1:hover > .search-btn {
   background: #4aae9b;
 }
 .search-btn {
@@ -58,17 +89,30 @@ data(){
   align-items: center;
   transition: 0.4s;
 }
-.search-text{
+.box1{
   border: none;
   background: none;
   outline: none;
-  float: left;
-  padding: 0;
+  padding: 2px;
   color: rgb(113,128,150);
-  font-size: 16px;
+  font-size: 15px;
+  transition: 0.4s;
+  width: 150px;
+}
+
+.box4{
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  padding: 2px;
+  color: rgb(113,128,150);
+  font-size: 20px;
   transition: 0.4s;
   line-height: 40px;
   width: 150px;
-  text-align: center;
+}
+
+.box4:hover {
+  cursor: pointer;
 }
 </style>
--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -6,6 +6,7 @@ import Home from './views/Home.vue'
 import Detail from './views/HousingDetail.vue'
 import AddRentalObjectForm from './components/addRentalObjectForm.vue'
 import TestPage from './views/TestPage.vue'
+import Overview from './views/Overview.vue'
 
 // create routes with views
 export default createRouter({
@@ -26,6 +27,10 @@ export default createRouter({
     {
       path: '/test-page',
       component: TestPage,
+    },
+    {
+      path: '/overview',
+      component: Overview,
     }
   ],
 })
