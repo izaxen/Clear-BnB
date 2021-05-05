cf87af93df85e6dbb1197a846db2eb11a5a45f2a
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-05-05 13:37:36 +0200

    fixed bug with select options

--- Frontend/src/components/Filter.vue
+++ Frontend/src/components/Filter.vue
@@ -3,16 +3,16 @@
     <h3>Filter</h3>
     <select v-model="city">
       <option value="">All cities</option>
-      <option v-for="object in objects" :key="object.id" :value="object.city">
-        {{ object.city }}
+      <option v-for="object in cityOption" :key="object.id" :value="object">
+        {{ object }}
       </option>
     </select>
     <input type="text" v-model="text" placeholder="text" />
     <label for="vol">Price {{ range }} kr</label>
     <input type="range" v-model="range" min="300" max="1500" step="10" />
-
     <input type="number" v-model="beds" min="1" placeholder="Number of beds" />
   </div>
+
   <RentalObject
     v-for="object in filterObjects"
     :key="object.id"
@@ -33,6 +33,7 @@ export default {
       text: '',
       city: '',
       objects: [],
+      cityOption: '',
       range: 900,
       beds: '',
     }
@@ -41,6 +42,16 @@ export default {
   async created() {
     await this.$store.dispatch('fetchRentalObjects')
     this.objects = await this.$store.state.rentalObjects
+    let unfilteredArr = []
+    this.objects.forEach((o) => {
+      unfilteredArr.push(o.city)
+    })
+
+    let removedDuplicates = unfilteredArr.filter((value, index) => {
+      return unfilteredArr.indexOf(value) === index
+    })
+    this.cityOption = removedDuplicates
+
     if (this.$store.state.searchObject) {
       this.city = this.$store.state.searchObject.city
       this.beds = this.$store.state.searchObject.guests
