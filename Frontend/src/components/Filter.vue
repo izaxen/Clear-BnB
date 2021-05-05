eba64f521cbb546f241ce592cda5d382d690d2be
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-05-05 11:04:35 +0200

    added functionallity to filter by beds and conected it with searchbar

--- Frontend/src/components/Filter.vue
+++ Frontend/src/components/Filter.vue
@@ -10,11 +10,13 @@
   <label for="vol">Price {{ range }} kr</label>
   <input type="range" v-model="range" min="300" max="1000" step="10" />
 
+  <input type="number" v-model="beds" min="1" placeholder="Number of beds" />
   <RentalObject
     v-for="object in filterObjects"
     :key="object.id"
     :object="object"
   />
+  <div v-if="filterObjects.length == 0">No match</div>
 </template>
 
 <script>
@@ -30,6 +32,7 @@ export default {
       city: '',
       objects: [],
       range: 600,
+      beds: '',
     }
   },
 
@@ -38,14 +41,19 @@ export default {
     this.objects = await this.$store.state.rentalObjects
     if (this.$store.state.searchObject) {
       this.city = this.$store.state.searchObject.city
+      this.beds = this.$store.state.searchObject.guests
       this.$store.commit('removeSearchObject')
     }
   },
 
   computed: {
     filterObjects: function () {
-      return this.filterObjectsByPrice(
-        this.filterObjectsByCity(this.filterObjectsByDescription(this.objects))
+      return this.filterObjectByBeds(
+        this.filterObjectsByPrice(
+          this.filterObjectsByCity(
+            this.filterObjectsByDescription(this.objects)
+          )
+        )
       )
     },
   },
@@ -56,7 +64,6 @@ export default {
         return objects
       }
       let aa = objects.filter((object) => object.city == this.city)
-      console.log(aa)
       return aa
     },
 
@@ -65,9 +72,14 @@ export default {
     },
 
     filterObjectsByPrice(objects) {
-      console.log(this.range)
       return objects.filter((object) =>
         object.price < this.range ? object : ''
+      )
+    },
+
+    filterObjectByBeds(objects) {
+      return objects.filter((object) =>
+        object.availableBeds >= this.beds ? object : ''
       )
     },
   },
